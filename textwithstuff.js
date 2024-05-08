ea4633758584772e22e29967e51cc44ad0039763
paro81 <129580920+paro81@users.noreply.github.com>
2024-05-07 22:23:44 -0400

    Update textwithstuff.js

--- textwithstuff.js
+++ textwithstuff.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
         // add messages below
             var messages = [
-              "in the works sometimes",
-              "pro casual geo dasher", 
-              "rhythm music is interesting", 
-              "the himothy is real", 
-              "idk what else to say", 
-              "don't go outside", 
-              "touch grass, have you seen a tree before?", 
-              "realising my errors since 2013", 
-              "minimal effort, high reward", 
-              "a lackluster of ideas, galore"
+                "in the works sometimes",
+                "pro casual geo dasher", 
+                "rhythm music is interesting", 
+                "the himothy is real", 
+                "idk what else to say", 
+                "don't go outside", 
+                "touch grass, have you seen a tree before?", 
+                "realising my errors since 2013", 
+                "minimal effort, high reward", 
+                "a lackluster of ideas, galore",
+                "somehow, in the end, i still exist."
             ];
         
             var randomIndex = Math.floor(Math.random() * messages.length);
