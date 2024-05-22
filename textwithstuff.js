c7adb605f17be755fbd36e9feca7cd22d3462ab6
paro81 <129580920+paro81@users.noreply.github.com>
2024-05-22 11:56:33 -0400

    Update textwithstuff.js

--- textwithstuff.js
+++ textwithstuff.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 "asdjjfgjsklafjlasfjasdkjgfhkdsafdjfkll kind of situation",
                 "ERROR 404 PAGE NOT FOUND",
                 "i hate circles, squares are okay, triangles are deadly & weird",
-                "watch it, i'm gonna cook soon"
+                "watch me, i'm gonna cook soon, just you wait."
             ];
         
             var randomIndex = Math.floor(Math.random() * messages.length);
