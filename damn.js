0a1d2b610b0d7ca24b5212b93c89916f6bc16280
paro81 <129580920+paro81@users.noreply.github.com>
2025-12-25 16:11:47 +0530

    Update damn.js

--- damn.js
+++ damn.js
@@ -1,43 +1,45 @@
 const cursorImg = document.getElementById("cursorImage");
 
-// ramdpm start 
 let x = Math.random() * window.innerWidth;
 let y = Math.random() * window.innerHeight;
 let targetX = x;
 let targetY = y;
+let angle = 0;
 
-//  initial 
 cursorImg.style.left = `${x}px`;
 cursorImg.style.top = `${y}px`;
 
-// maus
+// mouse
 document.addEventListener("mousemove", (e) => {
   targetX = e.clientX;
   targetY = e.clientY;
 
-  createTrail(x, y);
+  createTrail(x, y, angle);
 });
 
-// fllow
+// follow
 function animateCursor() {
   x += (targetX - x) * 0.15;
   y += (targetY - y) * 0.15;
 
+  angle += 6; // rotation speed
   cursorImg.style.left = `${x}px`;
   cursorImg.style.top = `${y}px`;
+  cursorImg.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
 
   requestAnimationFrame(animateCursor);
 }
 
 animateCursor();
 
-// tailralrailta
-function createTrail(x, y) {
+// trail
+function createTrail(cx, cy, parentAngle) {
   const trail = cursorImg.cloneNode(true);
-
   trail.className = "cursor-trail";
-  trail.style.left = `${x}px`;
-  trail.style.top = `${y}px`;
+
+  trail.style.left = `${cx}px`;
+  trail.style.top = `${cy}px`;
+  trail.style.transform = `translate(-50%, -50%) rotate(${parentAngle}deg)`;
 
   document.body.appendChild(trail);
 
@@ -45,7 +47,5 @@ function createTrail(x, y) {
     trail.style.opacity = "0";
   });
 
-  setTimeout(() => {
-    trail.remove();
-  }, 600);
+  setTimeout(() => trail.remove(), 600);
 }
