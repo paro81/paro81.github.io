27d6389250b5c2b5b57ca179f8732c568e972e42
paro81 <129580920+paro81@users.noreply.github.com>
2025-12-25 13:51:10 +0530

    Update damn.js

--- damn.js
+++ damn.js
@@ -1,1 +1,18 @@
+const cursorImg = document.getElementById("damn-bird.png");
+        
+let x = 0, y = 0;
+let targetX = 0, targetY = 0;
 
+document.addEventListener("mousemove", (e) => {
+  targetX = e.clientX;
+  targetY = e.clientY;
+});
+
+function animate() {
+  x += (targetX - x) * 0.15;
+  y += (targetY - y) * 0.15;
+  cursorImg.style.left = x + "px";
+  cursorImg.style.top = y + "px";
+  requestAnimationFrame(animate);
+}
+animate();
