831bec0a87fb934c3b1e38bb381dc385bcb4e37f
paro81 <129580920+paro81@users.noreply.github.com>
2026-08-30 13:07:44 +0530

    Update textwithstuff.js

--- textwithstuff.js
+++ textwithstuff.js
@@ -26,7 +26,8 @@ document.addEventListener("DOMContentLoaded", function() {
                 "i've completely lost all meaning",
                 "its like whatre you gonna do and then you just do it, you know",
                 "nothing ever happens anyway",
-                "where did we leave off? i'm not so sure..."
+                "where did we leave off? i'm not so sure...",
+                "vibecode my ass, bitch"
             ];
         
             var randomIndex = Math.floor(Math.random() * messages.length);
